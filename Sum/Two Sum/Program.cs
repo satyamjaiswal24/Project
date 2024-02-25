936f96e001ded3cd58b3f27411a8fc7cc4935f54
Satyam Jaiswal <satyamj@infinite.com>
2024-02-25 17:02:51 +0530

    initial commit

---
+++ Sum/Two Sum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Two_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 25;
+            Console.WriteLine(a + b);
+            Console.Read();
+        }
+    }
+}
